d025c99e84d1b7d96423ba89e749d072ed509db1
Allison Oliveira Silva <allisonsilva1999@gmail.com>
2025-07-03 06:21:40 -0300

    feat: create database migrations for core models

--- database/migrations/2025_07_02_000627_create_diet_plans_table.php
+++ database/migrations/2025_07_02_000627_create_diet_plans_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('diet_plans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('patient_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->integer('number_of_meals');
             $table->timestamps();
         });
     }
